893c9cb5244c291b79d384bb58985a75af82dc67
Shulayev1 <shulayev-a@mail.ru>
2025-12-27 05:03:45 +0000

    {Что делали}

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("login").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "flex"
+})
+
+document.getElementById("close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none"
+})
